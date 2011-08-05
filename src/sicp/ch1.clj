901365aaa2b86f9cd983c9672b91a5cfea486fe5
Paul Lam <paul@quantisan.com>
2011-08-05 15:14:25 -0400

    ex 1.8

--- src/sicp/ch1.clj
+++ src/sicp/ch1.clj
@@ -19,9 +19,18 @@ returns the sum of the squares of the two larger numbers."
     (and (< a b) (< a c))	(+ (* b b) (* c c))		; a is smallest
     (and (< b a) (< b c)) (+ (* a a) (* c c))		; b is smallest
     (and (< c a) (< c b)) (+ (* a a) (* b b))))	; c is smallest
-    
-;;;;;;;;;;;;
 
+;; ex 1.6
+(defn new-if
+  [predicate then-clause else-clause]
+	(cond 
+   predicate then-clause
+	 :else else-clause))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; ex 1.7, 1.8
+;;;;;;;;;;;;;;;;;;;;;;;;
 (defn abs
   [x]
   (if (< x 0)	
@@ -32,40 +41,46 @@ returns the sum of the squares of the two larger numbers."
   [x y]
 	(/ (+ x y) 2))
 
-(defn improve
-  [guess x]
-	(average guess (/ x guess)))
-
 (defn square [x] (* x x))
 
 (defn cube [x] (* x x x))
 
-(defn- good-enough?
+(defn- improve-sq
   [guess x]
-	(< (abs (- (square guess) x)) 0.001))
+	(average guess (/ x guess)))
 
-(defn- good-enough-frac?
+(defn- improve-cb
   [guess x]
-	(< (abs (- (square guess) x)) 
-     (abs (/ x 1000))))
+  (/ (+ (/ x (square guess))
+     		(* 2 guess))
+     3))
+
+(defn- good-enough?
+  [f]
+  (fn [guess x]
+		(< (abs (- (f guess) x)) 
+    	(abs (/ x 1000)))))
 
 (defn- sqrt-iter
-  [frac? guess x]
-	(if (if frac?
-      	(good-enough-frac? guess x)
-        (good-enough? guess x))
+  [guess x]
+	(if ((good-enough? square) guess x)
 		guess
-		(recur frac? (improve guess x) x)))
+		(recur (improve-sq guess x) x)))
+
+(defn- cbrt-iter
+  [guess x]
+	(if ((good-enough? cube) guess x)
+		guess
+		(recur (improve-cb guess x) x)))
 
 (defn sqrt
-  ([x & {:keys [guess frac?]
-         :or {guess 1.0, frac? true}}]	
-    (sqrt-iter frac? guess x)))
+  ([x & {:keys [guess]
+         :or {guess 1.0}}]	
+    (sqrt-iter guess x)))
 
-;; ex 1.6
-(defn new-if
-  [predicate then-clause else-clause]
-	(cond 
-   predicate then-clause
-	 :else else-clause))
+(defn cbrt
+  ([x & {:keys [guess]
+         :or {guess 1.0}}]	
+    (cbrt-iter guess x)))
+
 
