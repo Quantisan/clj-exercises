12684408e38e51078231a12f042815c214ce77bc
Paul Lam <paul@quantisan.com>
2011-08-18 20:07:01 -0400

    up to exercise 1.11

--- src/sicp/ch1_2.clj
+++ src/sicp/ch1_2.clj
@@ -41,14 +41,15 @@
 
 
 ;;;;
+;; pg 82
 
 (defn fib-rec
   [n]
   (cond 
     (= n 0) 0
     (= n 1) 1
-    :else (+ (fib (dec n))
-             (fib (- n 2)))))
+    :else (+ (fib-rec (dec n))
+             (fib-rec (- n 2)))))
 
 (defn fib-iter
   [n]
@@ -58,4 +59,61 @@
               b
               (recur (+ a b) a (dec count))))]
          (iter 1 0 n)))
-                
+
+;;;;;
+;; pg 84
+
+(defn first-denomination 
+  [kinds-of-coins]
+  (cond
+    (= kinds-of-coins 1) 1
+    (= kinds-of-coins 2) 5
+    (= kinds-of-coins 3) 10
+    (= kinds-of-coins 4) 25
+    (= kinds-of-coins 5) 50))
+
+(defn cc 
+  [amount kinds-of-coins]
+  (cond 
+    (= amount 0) 1
+    (or (< amount 0) (= kinds-of-coins 0)) 0
+    :else (+ (cc amount
+                 (- kinds-of-coins 1))
+             (cc (- amount
+                    (first-denomination kinds-of-coins))
+                 kinds-of-coins))))
+
+(defn count-change
+  [amount]
+  (cc amount 5))
+
+;; ex 1.11
+(defn f-rec
+  "A function f is defined by the rule that f (n) = n if
+n < 3 and f (n) = f (n - 1) + 2f (n - 2) + 3f (n - 3) if n >= 3. Write a
+procedure that computes f by means of a recursive process. Write
+a procedure that computes f by means of an iterative process."
+  [n]
+  (cond
+    (< n 3)  n
+    :else    (+ (f-rec (dec n))
+                (* 2 (f-rec (- n 2)))
+                (* 3 (f-rec (- n 3))))))
+
+(defn f-iter
+"f (n) = n if n < 3;
+and f (n) = f (n - 1) + 2f (n - 2) + 3f (n - 3) if n >= 3"
+  [n]
+  (letfn [(iter 
+            [a b c count]
+            (if (= count n)
+              a
+              (recur (+ a 
+                        (* 2 b)
+                        (* 3 c))
+                     a
+                     b
+                     (inc count))))]
+         (if (< n 3)
+           n
+           (iter 3 2 1 3))))
