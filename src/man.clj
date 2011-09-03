5a7df01658ee802be67f54b0476f8bbacdb667df
Paul Lam <paul@quantisan.com>
2011-09-03 11:42:18 -0400

    lazy-seq generate func and defn duplicates

--- src/man.clj
+++ src/man.clj
@@ -7,4 +7,10 @@
   "Generate a list of n random numbers each chosen from [0, k)."
   [n k]
   (when (> n 0)
-    (cons (rand-int k) (generate (dec n) k))))
+    (lazy-seq (cons (rand-int k) (generate (dec n) k)))))
+
+(defn duplicates
+  "Returns all the duplicates of key within coll."
+  [coll key]
+  (filter (partial = key) coll))
+
