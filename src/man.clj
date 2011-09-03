6f19ab7babdd0e994ae8240317694ffb486d6069
Paul Lam <paul@quantisan.com>
2011-09-03 12:02:48 -0400

    recursive solution

--- src/man.clj
+++ src/man.clj
@@ -1,8 +1,5 @@
 (ns man)
 
-;; Q: given a long collection of items, find all the duplicates.
-;;    only equal can be used, not comparable (i.e. no sorting).
-
 (defn generate
   "Generate a list of n random numbers each chosen from [0, k)."
   [n k]
@@ -14,3 +11,22 @@
   [coll key]
   (filter (partial = key) coll))
 
+(defn remove-dups
+  "Returns a coll without all the duplicates of key."
+  [coll key]
+  (remove (partial = key) coll))
+
+(defn sift-recur
+" Given a list of items, find all the duplicates.
+  Rules: - only equal can be used
+        - items are not comparable (i.e. no sorting)."
+  [coll]
+  (when-let [s (seq coll)]
+    (let [f      (first s)
+          r      (rest s)
+          dups   (duplicates s f)]
+      (if (seq dups)      
+        (lazy-seq (cons dups (sift-recur (remove-dups r f))))
+        (lazy-seq (sift-recur (remove-dups r f)))))))
+
+;; TODO parallelize
