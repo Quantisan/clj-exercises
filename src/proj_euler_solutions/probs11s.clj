c179e654226b8fead555803fde694cd4a3219341
Paul Lam <paul@quantisan.com>
2011-10-14 17:17:37 +0100

    Euler problem 16 solved

--- src/proj_euler_solutions/probs11s.clj
+++ src/proj_euler_solutions/probs11s.clj
@@ -1,2 +1,24 @@
-(ns euler.probs11s)
+(ns euler.probs11s
+    (:use clojure.math.numeric-tower))
 
+(defn int-to-digits-recur 
+  "Converts an integer to a list of its digits (in reverse)."
+  [x]
+  (if-not (zero? x)
+    (cons (rem x 10) (int-to-digits (floor (/ x 10))))))
+
+(defn int-to-digits-mapper
+  "Converts an integer to a list of its digits."
+  [x]
+  (letfn [(char-to-int
+            [c]
+            (Integer. (str c)))]                       
+  (map char-to-int (apply list (str x)))))
+
+(defn p16
+  ([]
+    (p16 1000))
+  ([pow]
+    (let [num (expt 2 pow)]
+      (reduce + (int-to-digits-mapper num)))))
+      
