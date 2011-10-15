88ef6b5178585a313992590c6fa9e0c87032a7b7
Paul Lam <paul@quantisan.com>
2011-10-15 16:06:29 +0100

    solved Euler #11

--- src/proj_euler_solutions/probs11s.clj
+++ src/proj_euler_solutions/probs11s.clj
@@ -1,11 +1,12 @@
 (ns euler.probs11s
-    (:use clojure.math.numeric-tower))
+    (:use clojure.math.numeric-tower
+          [clojure.string :only (split)]))
 
 (defn int-to-digits-recur 
   "Converts an integer to a list of its digits (in reverse)."
   [x]
   (if-not (zero? x)
-    (cons (rem x 10) (int-to-digits (floor (/ x 10))))))
+    (cons (rem x 10) (int-to-digits-recur (floor (/ x 10))))))
 
 (defn int-to-digits-mapper
   "Converts an integer to a list of its digits."
@@ -32,3 +33,87 @@
     (p20 100))
   ([x]
     (reduce + (int-to-digits-mapper (factorial x)))))
+
+(defn parse-str-numbers
+  [lat]
+  (into [] (map #(Integer/valueOf %) lat)))
+
+(def p11-grid
+  (let [grid  ["08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08"
+               "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00"
+               "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65"
+               "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91"
+               "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80"
+               "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50"
+               "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70"
+               "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21"
+               "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72"
+               "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95"
+               "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92"
+               "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57"
+               "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58"
+               "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40"
+               "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66"
+               "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69"
+               "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36"
+               "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16"
+               "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54"
+               "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"]]
+    (into []
+          (->> grid
+            (map #(split % #" "))
+            (map parse-str-numbers)))))
+
+(defn neighbours-down
+  [y x lim]
+  (for [y-idx  (range y (+ y 4))
+        :when (< y-idx lim)]
+    [y-idx x]))
+
+(defn neighbours-right
+  [y x lim]
+  (for [x-idx  (range x (+ x 4))
+        :when (< x-idx lim)]
+    [y x-idx]))
+
+(defn neighbours-downright
+  [y x lim]
+  (for [z  (range 0 4)
+        :let   [y-idx  (+ y z)
+                x-idx  (+ x z)]
+        :when  (and (< x-idx lim)
+                     (< y-idx lim))]
+    [y-idx x-idx]))
+
+(defn neighbours-upright
+  [y x lim]
+  (for [z  (range 0 4)
+        :let   [y-idx  (- y z)
+                x-idx  (+ x z)]
+        :when  (and (< x-idx lim)
+                    (>= y-idx 0))]
+    [y-idx x-idx]))
+
+(defn get-listed
+  [m coll]
+  (map #(get-in m %) coll))
+
+(defn eval-neighbours
+  [y x m f]
+  (let [lim    (count m)
+        coll   (for [g [neighbours-down neighbours-right neighbours-downright neighbours-upright]
+                     :let [idx  (g y x lim)]
+                     :when (= (count idx) 4)]
+                 (get-listed m idx))]
+      (map #(reduce f %) coll)))
+
+(defn p11
+  ([]
+    (p11 p11-grid *))
+  ([m f]
+    (let [dim     (count m)
+          results (for [x  (range dim)
+                        y  (range dim)]
+                    (eval-neighbours y x m f))]
+     (apply max (flatten results)))))
+      
