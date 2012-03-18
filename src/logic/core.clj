40bac5896cd2d5fe4f35e3c9a6f7329fa0a55131
Paul Lam <paul@quantisan.com>
2012-03-18 18:27:47 +0000

    started ch1 of reasoned schemer

--- src/logic/core.clj
+++ src/logic/core.clj
@@ -1,3 +1,47 @@
 (ns logic.core
+"
+  The Reasoned Schemer
+"  
   (:refer-clojure :exclude [==])
   (:use clojure.core.logic))
+
+;;; Chapter 1 ;;;
+(run* (q)
+      u#)
+
+(run* (q)
+      (== true q))
+
+(run* (q)
+      u#
+      (== true q))
+
+(run* (q)
+      s#
+      (== true q))
+
+(run* (r)
+      s#
+      (== 'corn r))
+
+(run* (r)
+      u#
+      (== 'corn r))
+
+(run* (q)
+      s#
+      (== false q))
+
+(run* (x)
+      (let [x  false]
+        (== true x)))
+
+(run* (x)
+      (let [x  false]
+        (== false x)))
+
+(run* (q)
+      (fresh (x)
+             (== true x)
+             (== true q)))
+        
