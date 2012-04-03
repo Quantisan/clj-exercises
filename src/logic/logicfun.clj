fb15844e32ff38da88fe135f4bf99b6366602069
Paul Lam <paul@quantisan.com>
2012-04-03 10:35:18 +0100

    logicfun part 1

---
+++ src/logic/logicfun.clj
@@ -0,0 +1,12 @@
+(ns logic.logicfun
+"
+  http://clojure.com/blog/2011/12/08/lojic-part-two.html
+"  
+  (:refer-clojure :exclude [==])
+  (:use clojure.core.logic))
+
+(run 1 [q])
+
+(run 1 [q] (== 4 q))
+
+(run 1 [q] (== 4 q) (== 5 q))
