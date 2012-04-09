87af9384c9fd761ba57c2b9ad57fd78fa23616e8
Paul Lam <paul@quantisan.com>
2012-04-09 11:18:20 +0100

    1.19

--- src/sicp/ch1_2.clj
+++ src/sicp/ch1_2.clj
@@ -182,3 +182,22 @@ and f (n) = f (n - 1) + 2f (n - 2) + 3f (n - 3) if n >= 3"
     (if (< cnt 2)
       (+ buf x)
       (recur buf (double x) (half cnt)))))
+
+; ex 1.19
+(defn fib-iter [a b p q count]
+  (println a b p q count)
+  (cond 
+    (zero? count) b
+    (even? count) (fib-iter a
+                            b 
+                            (+ (square p) (square q))
+                            (+ (square q) (* 2 p q))
+                            (half count))
+    :else (fib-iter (+ (* b q) (* a q) (* a p))
+                    (+ (* b p) (* a q))
+                    p
+                    q
+                    (dec count))))
+
+(defn fib [n]
+  (fib-iter 1 0 0 1 n))
