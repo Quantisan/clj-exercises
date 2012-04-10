264a50c4dcee69073a379ad0f6feee12698e8337
Paul Lam <paul.lam@forward.co.uk>
2012-04-10 18:34:43 +0100

    primality

--- src/sicp/ch1_2.clj
+++ src/sicp/ch1_2.clj
@@ -201,3 +201,16 @@ and f (n) = f (n - 1) + 2f (n - 2) + 3f (n - 3) if n >= 3"
 
 (defn fib [n]
   (fib-iter 1 0 0 1 n))
+
+; pg 98
+(defn divides? [a b]
+     (zero? (rem b a)))
+(defn find-divisor [n test-divisor]
+   (cond 
+     (> (square test-divisor) n) n
+     (divides? test-divisor n) test-divisor
+     :else (find-divisor n (inc test-divisor))))
+(defn smallest-divisor [n]
+  (find-divisor n 2))
+(defn prime? [n]
+  (= n (smallest-divisor n)))
