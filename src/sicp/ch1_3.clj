8cc0cabab7f76f7138c743707383a89259885c50
Paul Lam <paul.lam@forward.co.uk>
2012-04-14 16:20:19 +0100

    sicp 1.29

--- src/sicp/ch1_3.clj
+++ src/sicp/ch1_3.clj
@@ -18,3 +18,18 @@
 (defn integral [f a b dx]
   (let [add-dx  (fn [x] (+ x dx))]
     (* (sum f (+ a (/ dx 2.0)) add-dx b) dx)))
+
+; ex 1.29
+(defn simpson [f a b n]
+  {:pre [(even? n)]}
+  (let [h  (/ (- b a) n)
+        term (fn [k]
+               (let [c  (cond
+                          (zero? k) 1
+                          (= k n)   1
+                          (odd? k)  4
+                          (even? k) 2)]
+                 (* c
+                    (f (+ a (* k h))))))]
+    (* (/ h 3)
+       (sum term 0 inc n))))
