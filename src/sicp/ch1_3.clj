be6d9de016fbd7b34440eeec932d7372fbc55c7b
Paul Lam <paul@quantisan.com>
2012-04-14 14:41:57 +0100

    sec 1.3

---
+++ src/sicp/ch1_3.clj
@@ -0,0 +1,20 @@
+(ns sicp.ch1-3)
+
+(defn sum [term a next b]
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (next a) next b))))
+
+(defn cube [x]
+  (* x x x))
+
+(defn sum-cubes [a b]
+  (sum cube a inc b))
+
+(defn sum-integers [a b]
+  (sum identity a inc b))
+
+(defn integral [f a b dx]
+  (let [add-dx  (fn [x] (+ x dx))]
+    (* (sum f (+ a (/ dx 2.0)) add-dx b) dx)))
