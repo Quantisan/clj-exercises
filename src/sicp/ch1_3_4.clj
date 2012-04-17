fd00476a093b05492059a873241342ad1e61b0ff
Paul Lam <paul.lam@forward.co.uk>
2012-04-17 18:20:18 +0100

    deriv, newton-transform, newtons-method

--- src/sicp/ch1_3_4.clj
+++ src/sicp/ch1_3_4.clj
@@ -1,6 +1,7 @@
 (ns sicp.ch1-3-4
   (:use [sicp.ch1-3-3 :only (average fixed-point)]
-        [sicp.ch1-2 :only (square)]))
+        [sicp.ch1-2 :only (square)]
+        [sicp.ch1-1 :only (cube)]))
 
 (defn average-damp [f]
   (fn [x] (average x (f x))))
@@ -14,3 +15,17 @@
 (defn cube-root [x]
   (fixed-point (average-damp (fn [y] (/ x (square y))))
                1.0))
+
+(def dx 0.00001)
+
+(defn deriv [g]
+  (fn [x]
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(defn newton-transform [g]
+  (fn [x]
+    (- x (/ (g x) ((deriv g) x)))))
+
+(defn newtons-method [g guess]
+  (fixed-point (newton-transform g) guess))
