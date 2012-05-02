4a2721627e7eabda356386b931855c4eb2fcac83
Paul Lam <paul.lam@forward.co.uk>
2012-05-02 08:54:17 +0100

    init ch2.1.1, list struct data

---
+++ src/sicp/ch2_1_1.clj
@@ -0,0 +1,25 @@
+(ns sicp.ch2-1-1)
+
+(defn make-rat [n d] (cons n [d]))
+(defn numer [x] (first x))
+(defn denom [x] (last x))
+
+(defn add-rat [x y]
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(defn sub-rat [x y]
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(defn mul-rat [x y]
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+  
+(defn div-rat [x y]
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(defn equal-rat? [x y]     (= (* (numer x) (denom y))        (* (numer y) (denom x))))
