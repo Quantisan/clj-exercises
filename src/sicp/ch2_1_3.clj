02bd86d50d57c033bd53873d67c3e546a7e90681
Paul Lam <paul.lam@forward.co.uk>
2012-05-04 19:45:09 +0100

    sicp ex 2.5

--- src/sicp/ch2_1_3.clj
+++ src/sicp/ch2_1_3.clj
@@ -10,3 +10,16 @@
 (defn cdr [z]
   (z (fn [p q]
        q)))
+
+; ex 2.5
+(defn cons [a b]
+  (* (Math/pow 2 a) (Math/pow 3 b)))
+(defn pow-cnt [x n]
+  (loop [x x, n n, i 0]
+    (if (zero? (mod x n))
+      (recur (/ x n) n (inc i))
+      i)))
+(defn car [z]
+  (pow-cnt z 2))
+(defn cdr [z]
+  (pow-cnt z 3))
