28d64dd200ac860e984800371903dc6fdadf24bf
Paul Lam <paul@quantisan.com>
2012-05-07 14:39:31 +0100

    up to ex 2.10

--- src/sicp/ch2_1_3.clj
+++ src/sicp/ch2_1_3.clj
@@ -42,3 +42,35 @@
     (fn [x]
       (f (((comp a b) f) x)))))
 (((add one two) inc) 1)
+
+; ex 2.7
+(defn make-interval [a b] [a b])
+(defn upper-bound [x] (apply max x))
+(defn lower-bound [x] (apply min x))
+
+(defn add-interval [x y]
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+(defn mul-interval [x y]
+  (let [p1 (*(lower-bound x) (lower-bound y))
+        p2 (*(lower-bound x) (upper-bound y))
+        p3 (*(upper-bound x) (lower-bound y))
+        p4 (*(upper-bound x) (upper-bound y))]
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+; ex 2.10
+(defn div-interval [x y]
+  {:pre [(zero? (upper-bound y)) (zero? (lower-bound y))]}
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+; ex 2.8
+(defn sub-interval [x y]
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+; ex 2.9
+(defn width [x]
+  (/ (- (upper-bound x) (lower-bound x)) 2.0))
+                        
