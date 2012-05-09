1b4c7eb71924f8e8fc4a0e3dd3254a789e5fa798
Paul Lam <paul.lam@forward.co.uk>
2012-05-09 08:15:07 +0100

    ex 2.19

--- src/sicp/ch2_2_1.clj
+++ src/sicp/ch2_2_1.clj
@@ -17,3 +17,21 @@
   (when (seq coll)
     (cons (last coll) (reverse (drop-last coll)))))
 (= (reverse (list 1 4 9 16 25)) (list 25 16 9 4 1))
+
+; ex 2.19
+(defn first-denomination [coll]
+  (first coll))
+(defn except-first-denomination [coll]
+  (rest coll))
+(defn cc 
+  [amount coin-values]
+  (cond 
+    (= amount 0) 1
+    (or (< amount 0) (empty? coin-values)) 0
+    :else (+ (cc amount
+                 (except-first-denomination coin-values))
+             (cc (- amount
+                    (first-denomination coin-values))
+                 coin-values))))
+
+(def us-coins (list 50 25 10 5 1))(def uk-coins (list 100 50 20 10 5 2 1 0.5))
