16f099bfa729eab26a24f4b86193e8e3deda7db2
Paul Lam <paul.lam@forward.co.uk>
2012-05-09 08:19:10 +0100

    2.20

--- src/sicp/ch2_2_1.clj
+++ src/sicp/ch2_2_1.clj
@@ -35,3 +35,11 @@
                  coin-values))))
 
 (def us-coins (list 50 25 10 5 1))(def uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+; ex 2.20
+(defn same-parity [x & coll]
+  (cons x (if (even? x)
+            (filter even? coll)
+            (filter odd? coll))))
+(= (same-parity 1 2 3 4 5 6 7) '(1 3 5 7))
+(= (same-parity 2 3 4 5 6 7) '(2 4 6))
